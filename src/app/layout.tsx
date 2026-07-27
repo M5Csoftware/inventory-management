f6a6caa01369fdc11aaa642ed63e7b75c2129971
Altafraza07 <altafrzakgn@gmail.com>
2026-07-27 15:08:08 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Sidebar } from "@/components/layout/sidebar";
 import { Header } from "@/components/layout/header";
 import { InventoryProvider } from "@/context/inventory-context";
 import { AuthProvider } from "@/context/auth-context";
+import { TabProvider } from "@/context/tab-context";
 import { AuthGuard } from "@/components/auth-guard";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -36,16 +37,18 @@ export default function RootLayout({
         <AuthProvider>
           <AuthGuard>
             <InventoryProvider>
-              <div className="flex w-full min-h-screen">
-                <Sidebar />
-                <div className="flex-1 flex flex-col min-w-0">
-                  <Header />
-                  <main className="flex-1 overflow-y-auto min-h-0">
-                    {children}
-                  </main>
+              <TabProvider>
+                <div className="flex w-full min-h-screen">
+                  <Sidebar />
+                  <div className="flex-1 flex flex-col min-w-0">
+                    <Header />
+                    <main className="flex-1 overflow-y-auto min-h-0">
+                      {children}
+                    </main>
+                  </div>
                 </div>
-              </div>
-              <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} theme="colored" />
+                <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} theme="colored" />
+              </TabProvider>
             </InventoryProvider>
           </AuthGuard>
         </AuthProvider>
